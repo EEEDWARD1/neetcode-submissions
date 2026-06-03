f0b4b2f0e34ffc21e9ef2cff327b0743429c1b07
Ed <61796330+EEEDWARD1@users.noreply.github.com>
2026-06-03 17:44:29 +0100

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for (int num : nums){
+            if (!s.add(num)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
