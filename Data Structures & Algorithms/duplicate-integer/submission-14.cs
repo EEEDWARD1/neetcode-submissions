1f7ddd3ea1822d459c02b1509971acb7a5c98a12
Ed <61796330+EEEDWARD1@users.noreply.github.com>
2026-09-09 18:01:45 +0100

    Add: duplicate-integer - submission-14

---
+++ Data Structures & Algorithms/duplicate-integer/submission-14.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        if (nums.Length <= 1 || nums == null){
+            return false;
+        }
+
+        HashSet<int> duplicates = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++){
+            if(!duplicates.Add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
